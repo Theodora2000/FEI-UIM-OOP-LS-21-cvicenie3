aefe5be7310b94d555aa4ef0e1a67be0ef190707
Theodora2000 <xsimont@stuba.sk>
2021-03-02 15:39:36 +0100

    metody

--- src/main/java/sk/stuba/uim/fei/oop/Auto.java
+++ src/main/java/sk/stuba/uim/fei/oop/Auto.java
@@ -1,14 +1,15 @@
 package sk.stuba.uim.fei.oop;
 public class Auto {
 
-    public int pocetKolies;
-    public int pocetDvery;
-    public double kapacitaNadrze;
-    public String nazov;
+    private static final double FUEL_PER_KM=0.5;
+    private double kapacitaNadrze;
+    public double stavNadrze;
 
-    public int jedna;
-    int dva;//package private
-    protected int tri;
-    private int fou;
+    public void drive(double distance){
+        stavNadrze -= distance*FUEL_PER_KM;
+        if(stavNadrze<0){
+            stavNadrze=0;
+        }
+    }
 
 }
--- src/main/java/sk/stuba/uim/fei/oop/Main.java
+++ src/main/java/sk/stuba/uim/fei/oop/Main.java
@@ -3,16 +3,10 @@ package sk.stuba.uim.fei.oop;
 public class Main {
     public static void main(String[] args){
         Auto skoda = new Auto();
-        Auto bmw = new Auto();
 
-        skoda.jedna=1;
-        bmw.jedna=2;
-
-        if(skoda.jedna==bmw.jedna){
-            System.out.println("rovnake");
-        }else{
-            System.out.println("rozne");
-        }
+        skoda.stavNadrze = 100;
+        skoda.drive(100);
+        System.out.println(skoda.stavNadrze);
 
     }
 }
