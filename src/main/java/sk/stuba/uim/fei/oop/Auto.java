0b523e2fd4f66caa15497e62f86764f1ef75e3d4
Theodora2000 <xsimont@stuba.sk>
2021-03-02 15:55:45 +0100

    metoda

--- src/main/java/sk/stuba/uim/fei/oop/Auto.java
+++ src/main/java/sk/stuba/uim/fei/oop/Auto.java
@@ -4,12 +4,31 @@ public class Auto {
     private static final double FUEL_PER_KM=0.5;
     private double kapacitaNadrze;
     public double stavNadrze;
+    private boolean neojazdene;
 
     public void drive(double distance){
+        this.neojazdene=false;
         stavNadrze -= distance*FUEL_PER_KM;
         if(stavNadrze<0){
             stavNadrze=0;
         }
     }
-
+    public double getStavNadrze(){
+        return stavNadrze;
+    }
+    public void setStavNadrze(double stavNadrze){
+        this.stavNadrze = stavNadrze;
+    }
+    public String currentState(){
+        String result;
+        if(this.neojazdene){
+            result="Auto je neojazdene\n";
+        }
+        else
+            {
+                result="Auto je ozajdene";
+            }
+        result +="\n"+ stavNadrze+"\\"+kapacitaNadrze;
+        return  result;
+    }
 }
--- src/main/java/sk/stuba/uim/fei/oop/Main.java
+++ src/main/java/sk/stuba/uim/fei/oop/Main.java
@@ -3,10 +3,10 @@ package sk.stuba.uim.fei.oop;
 public class Main {
     public static void main(String[] args){
         Auto skoda = new Auto();
-
-        skoda.stavNadrze = 100;
+        System.out.println(skoda.currentState());
+        skoda.setStavNadrze(100);
         skoda.drive(100);
-        System.out.println(skoda.stavNadrze);
-
+        System.out.println(skoda.getStavNadrze());
+        System.out.println(skoda.currentState());
     }
 }
